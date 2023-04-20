dad188525cf598b9c65fe662b40e76e0bde0a191
양찬우 <isnow890@hotmail.com>
2023-04-20 16:53:50 +0900

    Riverpod 소개

---
+++ lib/screen/state_provider_screen.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_test/layout/default_layout.dart';
+import 'package:riverpod_test/riverpod/state_provider.dart';
+
+//StatlessWidget -> ConsumerWidget으로 변경
+class StateProviderScreen extends ConsumerWidget {
+  const StateProviderScreen({Key? key}) : super(key: key);
+
+  //WidgetRef 추가됨
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final provider = ref.watch(numberProvider);
+    return DefaultLayout(
+        title: 'StateProviderScreen',
+        body: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                provider.toString(),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  //값 업데이트
+                  ref
+                      .read(numberProvider.notifier)
+                      .update((state) => state + 1);
+                },
+                child: Text('UP'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  //값 업데이트
+                  Navigator.of(context)
+                      .push(MaterialPageRoute(builder: (_) => _NextScreen()));
+                },
+                child: Text('Next Screen'),
+              )
+            ],
+          ),
+        ));
+  }
+}
+
+class _NextScreen extends ConsumerWidget {
+  const _NextScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final provider = ref.watch(numberProvider);
+
+    return DefaultLayout(
+        title: 'StateProviderScreen',
+        body: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                provider.toString(),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  //값 업데이트
+                  //watch를 하고 있는 다른 위젯에서 같은 값을 공유하고 있음.
+                  ref
+                      .read(numberProvider.notifier)
+                      .update((state) => state + 1);
+                },
+                child: Text('UP'),
+              ),
+            ],
+          ),
+        ));
+  }
+}
