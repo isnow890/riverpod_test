d30676abdda2c6ea0c730456822f05cecb8a1fb7
isnow890 <isnow890@hotmail.com>
2023-04-23 00:45:59 +0900

    AutoDispose Modifier 실습

--- lib/riverpod/future_provider.dart
+++ lib/riverpod/future_provider.dart
@@ -5,7 +5,7 @@ final multipleFutureProvider = FutureProvider<List<int>>(
     await Future.delayed(
       Duration(seconds: 2),
     );
-    throw Exception('에러');
+    // throw Exception('에러');
     return [1, 2, 3, 4, 5];
   },
 );
---
+++ lib/screen/auto_dispose_modifier_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_test/layout/default_layout.dart';
+import 'package:riverpod_test/riverpod/auto_dispose_modifier_provider.dart';
+
+class AutoDisposeModifierScreen extends ConsumerWidget {
+  const AutoDisposeModifierScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final state = ref.watch(autoDisposeModifierProvider);
+
+    return DefaultLayout(
+        title: 'AutoDisposeModifierScreen',
+        body: Center(
+          child: state.when(
+              data: (data) {
+                return Text(data.toString());
+              },
+              error: (err, stk) {
+                Text(err.toString());
+              },
+              loading: () => CircularProgressIndicator()),
+        ));
+  }
+}
