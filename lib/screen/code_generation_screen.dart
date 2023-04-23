8924119b998b678ef24b68a4ff2b5b9e9f108b22
isnow890 <isnow890@hotmail.com>
2023-04-23 22:32:28 +0900

    Riverpod v2 Consumer 위젯 사용하기

--- lib/screen/code_generation_screen.dart
+++ lib/screen/code_generation_screen.dart
@@ -9,12 +9,13 @@ class CodeGenerationScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    print('build');
     //g.dart 안에 있는 프로바이더
     final state1 = ref.watch(gStateProvider);
     final state2 = ref.watch(gStateFutureProvider);
     final state3 = ref.watch(gStateFuture2Provider);
     final state4 = ref.watch(gStateMultiplyProvider(number1: 10, number2: 20));
-    final state5 = ref.watch(gStateNotifierProvider);
+
     return DefaultLayout(
       title: 'CodeGenerationScreen',
       body: Column(
@@ -56,7 +57,23 @@ class CodeGenerationScreen extends ConsumerWidget {
             },
           ),
           Text('State4 : $state4'),
-          Text('State4 : $state5'),
+          //해당 부분을 Consumer 위젯을 사용하면 이부분에서 변경사항이 있다면 상위 부분 재 빌드가 아니라 이부분만 재빌드가 됨.
+          //렌더링 효율이 좋아짐.
+          Consumer(
+            builder: (context, ref, child) {
+              final state5 = ref.watch(gStateNotifierProvider);
+              return Row(
+                children: [
+                  Text('State5: $state5'),
+                  //아래의 child 부분을 가져올 수 있음.
+                  //새로 렌더링하는 요소가 부분적일때 child에 state5에서도 한번만 실행되면 되는 위젯들을 밖에 빼놓고 안에서 재실행되면서 붙여쓸수 있게끔 기능 제공함.
+                  child!,
+                ],
+              );
+            },
+            child: Text('Hello'),
+          ),
+          // _StateFiveWidget(),
           Row(
             children: [
               ElevatedButton(
@@ -82,5 +99,16 @@ class CodeGenerationScreen extends ConsumerWidget {
         ],
       ),
     );
+  }
+}
+
+//부분적으로 빌드하고 싶을때. (Consumer 없이 사용할때)
+class _StateFiveWidget extends ConsumerWidget {
+  const _StateFiveWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final state5 = ref.watch(gStateNotifierProvider);
+    return Text('State4 : $state5');
   }
 }
